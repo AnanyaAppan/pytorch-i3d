78037ba2307fcbc7f76bfd11077a78bd8613b579
Ananya <ananya.appan@gmail.com>
2021-05-18 15:16:57 +0530

    removed print statements

--- train_i3d.py
+++ train_i3d.py
@@ -110,8 +110,8 @@ def run(init_lr=0.1, max_steps=64e3, mode='rgb', root='../../SSBD/ssbd_clip_segm
 
                 # compute classification loss (with max-pooling along time B x C x T)
                 cls_loss = F.binary_cross_entropy_with_logits(torch.max(per_frame_logits, dim=2)[0], torch.max(labels, dim=2)[0])
-                print(torch.max(per_frame_logits, dim=2)[0])
-                print(torch.max(labels, dim=2)[0])
+                # print(torch.max(per_frame_logits, dim=2)[0])
+                # print(torch.max(labels, dim=2)[0])
                 correct = torch.max(per_frame_logits, dim=2)[0].argmax(1).eq(torch.max(labels, dim=2)[0].argmax(1))
                 total += correct.float().sum().item() 
                 n += batch_size
